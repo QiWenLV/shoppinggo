6ce26c02d9a2d8757fb3aee38e2fa8049eecddd9
wendy <1477945122@qq.com>
2018-10-26 23:27:33 +0800

    1.1 -使用AngularJS搭建管理员后台的页面。并实现品牌列表的增删改查

--- shopinggo_manager_web/src/main/java/com/zqw/manager/controller/BrandController.java
+++ shopinggo_manager_web/src/main/java/com/zqw/manager/controller/BrandController.java
@@ -3,7 +3,9 @@ package com.zqw.manager.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.zqw.pojo.TbBrand;
 import com.zqw.sellergoods.service.BrandService;
-import org.springframework.stereotype.Controller;
+import entity.PageResult;
+import entity.Result;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/brand")
-//@Controller
 public class BrandController {
 
 	@Reference
@@ -25,5 +26,47 @@ public class BrandController {
 
 		return lt;
 	}
-	
+
+	@RequestMapping("/findOne")
+	public TbBrand findOne(Long id){
+		return brandService.findOne(id);
+	}
+
+	@RequestMapping("/findPage")
+	public PageResult findPage(int page, int size){
+		return brandService.findPage(page, size);
+	}
+
+	@RequestMapping("/add")
+	public Result add(@RequestBody TbBrand brand){
+
+		try {
+			brandService.add(brand);
+			return new Result(true, "增加成功");
+		}catch (Exception e){
+			return new Result(false, "增加失败");
+		}
+	}
+
+	@RequestMapping("/update")
+	public Result update(@RequestBody TbBrand brand){
+
+		try {
+			brandService.update(brand);
+			return new Result(true, "修改成功");
+		}catch (Exception e){
+			return new Result(false, "修改失败");
+		}
+	}
+
+	@RequestMapping("/delete")
+	public Result delete(Long[] ids){
+
+		try {
+			brandService.delete(ids);
+			return new Result(true, "删除成功");
+		}catch (Exception e){
+			return new Result(false, "删除失败");
+		}
+	}
 }
--- shopinggo_sellergoods_interface/src/main/java/com/zqw/sellergoods/service/BrandService.java
+++ shopinggo_sellergoods_interface/src/main/java/com/zqw/sellergoods/service/BrandService.java
@@ -2,6 +2,7 @@ package com.zqw.sellergoods.service;
 
 
 import com.zqw.pojo.TbBrand;
+import entity.PageResult;
 
 import java.util.List;
 
@@ -13,5 +14,31 @@ import java.util.List;
 public interface BrandService {
 
 	public List<TbBrand> findAll();
-	
+
+	public TbBrand findOne(Long id);
+
+	/**
+	 * 返回分页列表
+	 * @return
+	 */
+	public PageResult findPage(int pageNum, int pageSize);
+
+
+	/**
+	 * 添加
+	 * @param tbBrand
+	 */
+	public void add(TbBrand tbBrand);
+
+	/**
+	 * 修改
+	 * @param brand
+	 */
+	public void update(TbBrand brand);
+
+	/**
+	 * 删除
+	 * @param ids
+	 */
+	public void delete(Long[] ids);
 }
