a2a029516f979f8342e91a5fde6f2324e2cf892d
wendy <1477945122@qq.com>
2018-10-27 08:48:49 +0800

    1.1 -使用AngularJS搭建管理员后台的页面。并实现品牌列表的增删改查

--- shopinggo_sellergoods_service/src/main/java/com/zqw/sellergoods/service/impl/BrandServiceImpl.java
+++ shopinggo_sellergoods_service/src/main/java/com/zqw/sellergoods/service/impl/BrandServiceImpl.java
@@ -1,12 +1,14 @@
 package com.zqw.sellergoods.service.impl;
 
-import java.util.Arrays;
 import java.util.List;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.zqw.mapper.TbBrandMapper;
 import com.zqw.pojo.TbBrand;
 
 import com.zqw.sellergoods.service.BrandService;
+import entity.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.alibaba.dubbo.config.annotation.Service;
@@ -23,6 +25,38 @@ public class BrandServiceImpl implements BrandService {
 		return brandMapper.selectByExample(null);
 //		List<TbBrand> tbBrands = Arrays.asList(new TbBrand(), new TbBrand());
 //		return tbBrands;
+	}
+
+	@Override
+	public TbBrand findOne(Long id) {
+		return brandMapper.selectByPrimaryKey(id);
+	}
+
+	@Override
+	public PageResult findPage(int pageNum, int pageSize) {
+
+		PageHelper.startPage(pageNum, pageSize);
+		//分页查询所有
+		Page<TbBrand> page = (Page<TbBrand>) brandMapper.selectByExample(null);
+		//返回包装后的结果
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+
+    @Override
+    public void add(TbBrand tbBrand) {
+        brandMapper.insert(tbBrand);
+    }
+
+	@Override
+	public void update(TbBrand brand) {
+		brandMapper.updateByPrimaryKey(brand);
+	}
+
+	@Override
+	public void delete(Long[] ids) {
+		for(Long id : ids){
+			brandMapper.deleteByPrimaryKey(id);
+		}
 	}
 
 }
